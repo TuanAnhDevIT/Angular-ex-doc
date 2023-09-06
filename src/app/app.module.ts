0d033e8f083fea00b4de327b0c94bbab6b0179e5
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-09-06 16:13:16 +0700

    feat(admin): directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
+// app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive'; // Import your custom directive here
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective // Add your custom directive here
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
