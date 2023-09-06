e53d0f3347d0eb2b877db95c26926036051e9ec5
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-09-06 15:59:52 +0700

    feat(admin): pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title | uppercase }}</h1>
+    <p>{{ text | uppercase }}</p>
+  `
 })
 export class AppComponent {
-  title = 'Angular-doc';
+  title = 'Angular Pipe Demo';
+  text = 'This is an example of an uppercase pipe.';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UppercasePipe } from './uppercase.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UppercasePipe
   ],
   imports: [
     BrowserModule,
