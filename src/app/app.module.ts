a5295db47202f820c92663f125fd8c1270138a12
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-09-06 11:18:05 +0700

    feat(admin): content projection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  ZippyComponent,
+  ZippyContentDirective,
+  ZippyToggleDirective,
+} from './example-zippy.component';
+import { ZippyBasicComponent } from './zippy-basic/zippy-basic.component';
+import { ZippyMultislotComponent } from './zippy-multislot/zippy-multislot.component';
+import { ZippyNgprojectasComponent } from './zippy-ngprojectas/zippy-ngprojectas.component';
 
 @NgModule({
+  imports: [BrowserModule, FormsModule],
   declarations: [
-    AppComponent
+    AppComponent,
+    ZippyComponent,
+    ZippyToggleDirective,
+    ZippyContentDirective,
+    ZippyBasicComponent,
+    ZippyMultislotComponent,
+    ZippyNgprojectasComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
